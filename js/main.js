3475ff6a4fc8634518ca8eab769472b92522f9dd
Adrian Bece <adrian.bece@inchoo.net>
2017-03-25 21:14:09 +0100

    Revamp - Work in progress

--- js/main.js
+++ js/main.js
@@ -1,26 +1,93 @@
-jQuery(document).ready(function($){
-	var timelineBlocks = $('.cd-timeline-block'),
-		offset = 0.8;
+function toggleContainerClassAndHideButton(c, b){
+    toggleContainerClass(c);
+    hideButton(b);
+}
 
-	//hide timeline blocks which are outside the viewport
-	hideBlocks(timelineBlocks, offset);
+function toggleContainerClass(c) {
+    var container = document.getElementById(c);
+    container.className += " active";
+}
 
-	//on scolling, show/animate timeline blocks when enter the viewport
-	$(window).on('scroll', function(){
-		(!window.requestAnimationFrame) 
-			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
-	});
+function hideButton(b) {
+    var buttonContainer = b.parentNode
+    buttonContainer.className += " hidden";
+}
 
-	function hideBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-		});
-	}
+(function() {
+    'use strict';
 
-	function showBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-		});
-	}
-});
+    // Feature Test
+    if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
+
+        // Function to animate the scroll
+        var smoothScroll = function (anchor, duration) {
+
+            // Calculate how far and how fast to scroll
+            var startLocation = window.pageYOffset;
+            var endLocation = anchor.offsetTop;
+            var distance = endLocation - startLocation;
+            var increments = distance/(duration/16);
+            var stopAnimation;
+
+            // Scroll the page by an increment, and check if it's time to stop
+            var animateScroll = function () {
+                window.scrollBy(0, increments);
+                stopAnimation();
+            };
+
+            // If scrolling down
+            if ( increments >= 0 ) {
+                // Stop animation when you reach the anchor OR the bottom of the page
+                stopAnimation = function () {
+                    var travelled = window.pageYOffset;
+                    if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+                        clearInterval(runAnimation);
+                    }
+                };
+            }
+            // If scrolling up
+            else {
+                // Stop animation when you reach the anchor OR the top of the page
+                stopAnimation = function () {
+                    var travelled = window.pageYOffset;
+                    if ( travelled <= (endLocation || 0) ) {
+                        clearInterval(runAnimation);
+                    }
+                };
+            }
+
+            // Loop the animation function
+            var runAnimation = setInterval(animateScroll, 16);
+
+        };
+
+        // Define smooth scroll links
+        var scrollToggle = document.querySelectorAll('.scroll');
+
+        // For each smooth scroll link
+        [].forEach.call(scrollToggle, function (toggle) {
+
+            // When the smooth scroll link is clicked
+            toggle.addEventListener('click', function(e) {
+
+                // Prevent the default link behavior
+                e.preventDefault();
+
+                // Get anchor link and calculate distance from the top
+                var dataID = toggle.getAttribute('href');
+                var dataTarget = document.querySelector(dataID);
+                var dataSpeed = toggle.getAttribute('data-speed');
+
+                // If the anchor exists
+                if (dataTarget) {
+                    // Scroll to the anchor
+                    smoothScroll(dataTarget, dataSpeed || 500);
+                }
+
+            }, false);
+
+        });
+
+    }
+
+})();
