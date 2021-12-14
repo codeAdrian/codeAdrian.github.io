d4cd5031ba27c8c8a21080dfd6ff0362a5c28e9d
Adrian Bece <adrian@prototyp.digital>
2021-12-14 22:59:30 +0100

    Slider

--- src/assets/js/glideInit.js
+++ src/assets/js/glideInit.js
@@ -1,1 +1,16 @@
-new Glide(".glide").mount();
+const carouselAbout = new Glide("#js-about", {
+  perView: 3,
+  bound: true,
+  gap: 0,
+  touchRatio: 0,
+  type: "slider",
+  rewind: false,
+  breakpoints: {
+    1280: {
+      touchRatio: 0.5,
+      perView: 1,
+    },
+  },
+});
+
+carouselAbout.mount();
